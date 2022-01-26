a13f909bc00132fad2819194845f91139cd2b47d
jonssonyan <yz808@outlook.com>
2022-01-27 01:23:50 +0800

    init

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <el-breadcrumb class='app-breadcrumb' separator='/'>
-    <transition-group name='breadcrumb'>
-      <el-breadcrumb-item v-for='(item,index) in levelList' :key='item.path'>
-        <span v-if="item.redirect==='noRedirect'||index==levelList.length-1" class='no-redirect'>{{
-            item.meta.title
-          }}</span>
-        <a v-else @click.prevent='handleLink(item)'>{{ item.meta.title }}</a>
+  <el-breadcrumb class="app-breadcrumb" separator="/">
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
+        <span
+          v-if="item.redirect === 'noRedirect' || index == levelList.length - 1"
+          class="no-redirect"
+          >{{ item.meta.title }}</span
+        >
+        <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
       </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
@@ -31,14 +33,18 @@ export default {
   methods: {
     getBreadcrumb() {
       // only show routes with meta.title
-      let matched = this.$route.matched.filter(item => item.meta && item.meta.title)
+      let matched = this.$route.matched.filter(
+        (item) => item.meta && item.meta.title
+      )
       const first = matched[0]
 
       if (!this.isDashboard(first)) {
-        matched = [{ path: '/', meta: { title: '仪表盘' } }].concat(matched)
+        matched = [{ path: '/' }].concat(matched)
       }
 
-      this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
+      this.levelList = matched.filter(
+        (item) => item.meta && item.meta.title && item.meta.breadcrumb !== false
+      )
     },
     isDashboard(route) {
       const name = route && route.name
@@ -65,7 +71,7 @@ export default {
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .app-breadcrumb.el-breadcrumb {
   display: inline-block;
   font-size: 14px;
