215258b27af2e1e464e40f8cf517c540a2c2ec4d
jonssonyan <yz808@outlook.com>
2022-04-19 22:42:30 +0800

    fix: obfuscator

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const defaultSettings = require('./src/settings.js')
-const JavaScriptObfuscator = require('webpack-obfuscator');
+const JavaScriptObfuscator = require('webpack-obfuscator')
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -40,38 +40,12 @@ module.exports = {
     },
     plugins: [
       // 最佳性能：性能比没有模糊处理慢 30-35% 配置项参考：https://github.com/javascript-obfuscator/javascript-obfuscator
-      new JavaScriptObfuscator({
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.75,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        debugProtection: false,
-        debugProtectionInterval: 0,
-        disableConsoleOutput: true,
-        identifierNamesGenerator: 'hexadecimal',
-        log: false,
-        numbersToExpressions: true,
-        renameGlobals: false,
-        selfDefending: true,
-        simplify: true,
-        splitStrings: true,
-        splitStringsChunkLength: 10,
-        stringArray: true,
-        stringArrayCallsTransform: true,
-        stringArrayCallsTransformThreshold: 0.75,
-        stringArrayEncoding: ['base64'],
-        stringArrayIndexShift: true,
-        stringArrayRotate: true,
-        stringArrayShuffle: true,
-        stringArrayWrappersCount: 2,
-        stringArrayWrappersChainedCalls: true,
-        stringArrayWrappersParametersMaxCount: 4,
-        stringArrayWrappersType: 'function',
-        stringArrayThreshold: 0.75,
-        transformObjectKeys: true,
-        unicodeEscapeSequence: false
-      }, [])
+      new JavaScriptObfuscator(
+        {
+          rotateStringArray: true
+        },
+        []
+      )
     ]
   },
   chainWebpack(config) {
