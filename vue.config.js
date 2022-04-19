18cc0d1b9cbd01303cea8d93aab56370a3726002
jonssonyan <yz808@outlook.com>
2022-04-19 17:29:52 +0800

    feat: obfuscator

--- vue.config.js
+++ vue.config.js
@@ -39,8 +39,38 @@ module.exports = {
       }
     },
     plugins: [
+      // 最佳性能：性能比没有模糊处理慢 30-35% 配置项参考：https://github.com/javascript-obfuscator/javascript-obfuscator
       new JavaScriptObfuscator({
-        rotateStringArray: true,
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: true,
+        renameGlobals: false,
+        selfDefending: true,
+        simplify: true,
+        splitStrings: true,
+        splitStringsChunkLength: 10,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.75,
+        stringArrayEncoding: ['base64'],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 2,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 4,
+        stringArrayWrappersType: 'function',
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false
       }, [])
     ]
   },
