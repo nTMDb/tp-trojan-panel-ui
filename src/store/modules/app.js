04ecdbcdf090e9bb03727eab9ba63b32bf1780b3
jonssonyan <yz808@outlook.com>
2022-05-08 02:14:47 +0800

    fix

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -28,6 +28,10 @@ const mutations = {
   },
   TOGGLE_DEVICE: (state, device) => {
     state.device = device
+  },
+  SET_SIZE: (state, size) => {
+    state.size = size
+    Cookies.set('size', size)
   }
 }
 
@@ -40,6 +44,9 @@ const actions = {
   },
   toggleDevice({ commit }, device) {
     commit('TOGGLE_DEVICE', device)
+  },
+  setSize({ commit }, size) {
+    commit('SET_SIZE', size)
   }
 }
 
