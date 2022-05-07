e847d7f57c28270421132b79de379996db87cc8b
jonssonyan <yz808@outlook.com>
2022-05-08 03:16:06 +0800

    fix: setting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-
-import VueClipboard from 'vue-clipboard2'
 import 'normalize.css/normalize.css'
 import ElementUI from 'element-ui'
 import '@/styles/element-variables.scss'
@@ -9,12 +7,15 @@ import '@/styles/index.scss'
 import App from './App'
 import store from '@/store'
 import router from '@/router'
-
+import Cookies from 'js-cookie'
+import enLang from 'element-ui/lib/locale/lang/en' // 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
 import '@/icons'
 import '@/permission'
 
-Vue.use(VueClipboard)
-Vue.use(ElementUI)
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'small', // set element-ui default size
+  locale: enLang // 如果使用中文，无需设置，请删除
+})
 
 Vue.config.productionTip = false
 
