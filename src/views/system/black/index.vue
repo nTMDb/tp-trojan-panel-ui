28b68c6bc640fd8eed21b6fe47228efb9c599e92
jonssonyan <yz808@outlook.com>
2022-05-09 10:49:50 +0800

    feat: black list

--- src/views/system/black/index.vue
+++ src/views/system/black/index.vue
@@ -1,13 +1,227 @@
 <template>
+  <div class='app-container'>
+    <div class='filter-container'>
+      <el-input
+        v-model='listQuery.ip'
+        :placeholder="$t('table.blackListIp')"
+        style='width: 200px'
+        class='filter-item'
+        clearable
+        @keyup.enter.native='handleFilter'
+        @clear='handleFilter'
+      />
+      <el-button
+        class='filter-item'
+        type='primary'
+        icon='el-icon-search'
+        @click='handleFilter'
+      >
+        {{ $t('table.search') }}
+      </el-button>
+      <el-button
+        class='filter-item'
+        style='margin-left: 10px'
+        type='primary'
+        icon='el-icon-edit'
+        @click='handleCreate'
+        v-permission="['sysadmin']"
+      >
+        {{ $t('table.add') }}
+      </el-button>
+    </div>
+    <el-table
+      :key='tableKey'
+      v-loading='listLoading'
+      :data='list'
+      border
+      fit
+      highlight-current-row
+      style='width: 100%'
+    >
+      <el-table-column
+        :label="$t('table.id')"
+        sortable='custom'
+        align='center'
+        width='80'
+        type='index'
+      />
+      <el-table-column :label="$t('table.blackListIp')" width='180' align='center'>
+        <template slot-scope='{ row }'>
+          <span>{{ row.ip }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="$t('table.createTime')"
+        width='180'
+        align='center'
+      >
+        <template slot-scope='{ row }'>
+          <span>{{ timeStampToDate(row.createTime, false) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="$t('table.actions')"
+        align='center'
+        class-name='small-padding fixed-width'
+      >
+        <template slot-scope='{ row, $index }'>
+          <el-button
+            size='mini'
+            type='danger'
+            @click='handleDelete(row, $index)'
+            v-permission="['sysadmin']"
+          >
+            {{ $t('table.delete') }}
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
+    <pagination
+      v-show='total > 0'
+      :total='total'
+      :page.sync='listQuery.pageNum'
+      :limit.sync='listQuery.pageSize'
+      @pagination='getList'
+    />
+
+    <el-dialog :title="$t('table.add')" :visible.sync='dialogFormVisible'>
+      <el-form
+        ref='dataForm'
+        :rules='createRules'
+        :model='temp'
+        label-position='left'
+      >
+        <el-form-item :label="$t('table.blackListIp')" prop='ip' clearable>
+          <el-input v-model='temp.ip' />
+        </el-form-item>
+      </el-form>
+      <div slot='footer' class='dialog-footer'>
+        <el-button @click='dialogFormVisible = false'
+        >{{ $t('table.cancel') }}
+        </el-button>
+        <el-button
+          type='primary'
+          @click='createData()'
+        >
+          {{ $t('table.confirm') }}
+        </el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { timeStampToDate } from '@/utils'
+import Pagination from '@/components/Pagination'
+import { MessageBox } from 'element-ui'
+import permission from '@/directive/permission/index.js'
+import { createBlackList, deleteBlackListByIp, selectBlackListPage } from '@/api/black-list' // 权限判断指令
+
 export default {
-  name: "index"
-};
+  name: 'Black',
+  components: { Pagination },
+  directives: { permission },
+  data() {
+    return {
+      tableKey: 0,
+      listLoading: true,
+      list: null,
+      total: 0,
+      listQuery: {
+        pageNum: 1,
+        pageSize: 20,
+        ip: ''
+      },
+      temp: {
+        id: undefined,
+        ip: '',
+        createTime: new Date()
+      },
+      dialogFormVisible: false,
+      createRules: {
+        ip: [
+          { required: true, message: '请输入IP', trigger: 'change' },
+          {
+            min: 3,
+            max: 64,
+            message: 'IP的范围在3-64字符之间',
+            trigger: 'change'
+          }
+        ]
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    timeStampToDate,
+    getList() {
+      this.listLoading = true
+      selectBlackListPage(this.listQuery).then((response) => {
+        this.list = response.data.blackLists
+        this.total = response.data.total
+
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
+      })
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        ip: '',
+        createTime: new Date()
+      }
+    },
+    handleFilter() {
+      this.listQuery.pageNum = 1
+      this.getList()
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    handleDelete(row, index) {
+      MessageBox.confirm('确认删除该IP？', '警告', {
+        confirmButtonText: '是',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        const tempData = Object.assign({}, row)
+        deleteBlackListByIp(tempData).then(() => {
+          this.list.splice(index, 1)
+          this.$notify({
+            title: 'Success',
+            message: '删除成功',
+            type: 'success',
+            duration: 2000
+          })
+        })
+      })
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          createBlackList(this.temp).then(() => {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: '创建成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
