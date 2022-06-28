721d5d4008c3c1451c77d013b09cb71e7a00bd1c
jonssonyan <yz808@outlook.com>
2022-06-28 14:22:26 +0800

    fix: Docker

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ init_var() {
 
   trojan_panel_ui_version=latest
 
-  arch_arr=('amd64' 'arm64')
-
-  touch Dockerfile
+  arch_arr="linux/amd64,linux/arm64"
 }
 
 echo_content() {
@@ -39,46 +37,25 @@ echo_content() {
 }
 
 main() {
-  for get_arch in ${arch_arr[*]}; do
-    echo_content skyBlue "开始构建trojan-panel-ui-linux-${get_arch}"
+  echo_content skyBlue "start build trojan-panel-ui CPU架构：${arch_arr}"
 
-    cat >Dockerfile <<EOF
-FROM nginx:1.20-alpine
-LABEL maintainer="jonsosnyan <https://jonssonyan.com>"
-RUN mkdir -p /tpdata/trojan-panel-ui/
-WORKDIR /tpdata/trojan-panel-ui/
-COPY dist/ .
-ENTRYPOINT nginx -g 'daemon off;'
-EXPOSE 80
-EOF
+  docker buildx build -t jonssonyan/trojan-panel-ui:latest --platform ${arch_arr} --push .
+  if [[ "$?" == "0" ]]; then
+    echo_content green "trojan-panel-ui Version：latest CPU架构：${arch_arr} build success"
 
-    docker buildx build -t jonssonyan/trojan-panel-ui-linux-"${get_arch}" --platform linux/"${get_arch}" --load .
-    if [[ "$?" == "0" ]]; then
-      echo_content green "trojan-panel-ui-linux-${get_arch}构建成功"
-      echo_content skyBlue "开始推送trojan-panel-ui-linux-${get_arch}"
-      docker image tag jonssonyan/trojan-panel-ui-linux-"${get_arch}":latest jonssonyan/trojan-panel-ui:latest && \
-      docker image push jonssonyan/trojan-panel-ui:latest && \
-      docker rmi -f jonssonyan/trojan-panel-ui:latest
+    if [[ ${trojan_panel_ui_version} != "latest" ]]; then
+      docker buildx build -t jonssonyan/trojan-panel-ui:${trojan_panel_ui_version} --platform ${arch_arr} --push .
       if [[ "$?" == "0" ]]; then
-        echo_content green "镜像名称：jonssonyan/trojan-panel-ui:latest 架构：${get_arch}推送成功"
+        echo_content green "trojan-panel-ui-linux Version：${trojan_panel_ui_version} CPU架构：${arch_arr} build success"
       else
-        echo_content red "镜像名称：jonssonyan/trojan-panel-ui:latest 架构：${get_arch}推送失败"
+        echo_content red "trojan-panel-ui-linux Version：${trojan_panel_ui_version} CPU架构：${arch_arr} build failed"
       fi
-
-      if [[ ${trojan_panel_ui_version} != "latest" ]]; then
-        docker image tag jonssonyan/trojan-panel-ui-linux-"${get_arch}":latest jonssonyan/trojan-panel-ui:${trojan_panel_ui_version} && \
-        docker image push jonssonyan/trojan-panel-ui:${trojan_panel_ui_version} && \
-        docker rmi -f jonssonyan/trojan-panel-ui:${trojan_panel_ui_version}
-        if [[ "$?" == "0" ]]; then
-          echo_content green "镜像名称：jonssonyan/trojan-panel-ui:${trojan_panel_ui_version} 架构：${get_arch}推送成功"
-        else
-          echo_content green "镜像名称：jonssonyan/trojan-panel-ui:${trojan_panel_ui_version} 架构：${get_arch}推送成功"
-        fi
-      fi
-    else
-      echo_content red "trojan-panel-ui-linux-${get_arch}构建失败"
     fi
-  done
+  else
+    echo_content red "trojan-panel-ui-linux Version：latest CPU架构：${arch_arr} build failed"
+  fi
+
+  echo_content skyBlue "trojan-panel-ui CPU架构：${arch_arr} build finished"
 }
 
 init_var
