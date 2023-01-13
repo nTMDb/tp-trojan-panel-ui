e03a403323117dcbf3eea9be12a5feac529164c8
jonssonyan <yz808@outlook.com>
2023-01-13 11:01:36 +0800

    fix: title

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -2,9 +2,16 @@ import defaultSettings from '@/settings'
 
 const title = defaultSettings.title || 'Trojan Panel'
 
+// export default function getPageTitle(pageTitle) {
+//   if (pageTitle) {
+//     return `${pageTitle} - ${title}`
+//   }
+//   return `${title}`
+// }
+
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
-    return `${pageTitle} - ${title}`
+    return `${pageTitle}`
   }
-  return `${title}`
+  return ``
 }
