e9028b70c4e1c212f10079079dd4df6d299619ff
jonssonyan <yz808@outlook.com>
2023-02-15 16:59:56 +0800

    Revert "fix: bug"
    This reverts commit 3882904bbb7f150188b98c82acb145acafb444f1.

--- src/views/node/list/index.vue
+++ src/views/node/list/index.vue
@@ -551,7 +551,7 @@ export default {
       return this.isXray && this.temp.xrayProtocol === 'shadowsocks'
     },
     showXrayFlow() {
-      return (this.isXrayVless || this.isXrayTrojan) && this.temp.xrayStreamSettingsEntity.security === 'xtls'
+      return this.isXrayVless || this.isXrayTrojan
     },
     isTrojanGo() {
       return getNodeTypeName(this.temp.nodeTypeId) === 'trojan-go'
