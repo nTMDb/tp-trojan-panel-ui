f4d46701d1e6e3fab039a69ee2fd7c773b641a14
jonssonyan <yz808@outlook.com>
2023-02-23 19:34:46 +0800

    fix: v2.0.3 bug

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,9 +1,9 @@
 <template>
   <el-breadcrumb class="app-breadcrumb" separator="/">
     <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
+      <el-breadcrumb-item v-for="(item, index) in levelList" :key="index">
         <span
-          v-if="item.redirect === 'noRedirect' || index == levelList.length - 1"
+          v-if="item.redirect === 'noRedirect' || index === levelList.length - 1"
           class="no-redirect"
         >
           {{ generateTitle(item.meta.title) }}
