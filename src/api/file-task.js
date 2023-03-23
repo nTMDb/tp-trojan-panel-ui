db3df8194326a7317912ee3fdb555603ccca323b
jonssonyan <yz808@outlook.com>
2023-03-23 18:00:33 +0800

    fix: v2.0.4

---
+++ src/api/file-task.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+/**
+ * 分页查询文件任务
+ * @param data
+ * @returns
+ */
+export function selectFileTaskPage(data) {
+  return request({
+    url: '/fileTask/selectFileTaskPage',
+    method: 'get',
+    params: data
+  })
+}
+
+/**
+ * 删除文件任务
+ * @param data
+ * @returns
+ */
+export function deleteFileTaskById(data) {
+  return request({
+    url: '/fileTask/deleteFileTaskById',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 下载文件任务的文件
+ * @param data
+ * @returns
+ */
+export function downloadFileTask(data) {
+  return request({
+    url: '/fileTask/downloadFileTask',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 获取文件模板
+ * @param data
+ * @returns
+ */
+export function downloadCsvTemplate(data) {
+  return request({
+    url: '/fileTask/downloadCsvTemplate',
+    method: 'post',
+    data
+  })
+}
