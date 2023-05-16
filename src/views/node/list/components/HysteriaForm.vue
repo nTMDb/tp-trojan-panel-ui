2ff13cfc5046870a15eb11bea99e5c91d26d9df6
jonssonyan <yz808@outlook.com>
2023-05-16 10:50:09 +0800

    fix: v2.1.2

--- src/views/node/list/components/HysteriaForm.vue
+++ src/views/node/list/components/HysteriaForm.vue
@@ -68,7 +68,7 @@ export default {
   },
   data() {
     return {
-      hysteriaProtocols: ['udp', 'faketcp']
+      hysteriaProtocols: ['udp', 'wechat-video', 'faketcp']
     }
   },
   methods: {
